59383887bd6e35d89bf8fc43f2dbba62675e88ca
Manikandan312003 <106978783+Manikandan312003@users.noreply.github.com>
2023-07-11 14:16:47 +0530

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,19 +1,61 @@
-class laptop():
-    def print(self):
-        print("Laptop")
+class libraryItem():
+    def __int__(self):
+        self.__id=0;
+        self.__title='';
+        self.__year=0;
 
-class Desktop():
-    def print(self):
-        print("Desktop")
+    def getid(self):
+        return self.__id
 
-class dev():
-    def print(self,java):
-        try:
-            (java.print())
-        except:
-            print("The object you passed not belongs to the laptop or Desktop object")
-lap=laptop()
-des=Desktop()
-d=dev()
-d.print(lap)
-d.print(des)
+    def gettitle(self):
+        return self.__title
+
+    def getyear(self):
+        return self.__year
+
+    def setid(self,id):
+        self.__id=id
+
+    def settitle(self,title):
+        self.__title=title
+
+    def setyear(self,year):
+        self.__year=year
+
+
+
+
+
+class Book(libraryItem):
+    def __int__(self):
+        self.author=''
+
+    def dispBook(self, id, title, year, author):
+        print(id,title,year,author)
+
+
+
+
+
+class Magazie(libraryItem):
+    def __int__(self):
+        self.issue=0
+
+    def dispMagazie(self, id, title, year, issue):
+        print(id,title,year,issue)
+
+
+
+alchemist = Book()
+alchemist.setid (123)
+alchemist.settitle ('The Alchemist')
+alchemist.setyear (2001)
+alchemist.author = ('Paul')
+alchemist.dispBook(alchemist.getid(), alchemist.gettitle(), alchemist.getyear(), alchemist.author)
+
+fortNight=Magazie()
+fortNight.setid( 12)
+fortNight.settitle( 'The FortNight')
+fortNight.setyear( 1992)
+fortNight.issue = 7
+fortNight.dispMagazie(fortNight.getid(), fortNight.gettitle(), fortNight.getyear(), fortNight.issue)
